dd406b1830ce2a21dc7bc55dbe4492c948becd88
AaronPrince33 <aaron.rayyan7718@gmail.com>
2026-09-23 14:08:33 +0700

    loadingScreen

--- EnemyWave/EnemyWave/Program.cs
+++ EnemyWave/EnemyWave/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿for (int i = 1; i <= 100; i++)
+{
+    Console.WriteLine($"Enemy Wave {i} has started");
+    Console.WriteLine($"Enemy Wave {i} is cleared\n");
+}
+;
---
+++ LoadingScreen/LoadingScreen/Program.cs
@@ -0,0 +1,8 @@
+﻿int totalSteps = 5;
+
+for (int i = 1; i <= 5; i += 1)
+{
+    Console.WriteLine($"Loading.. Step" + i + "of " + totalSteps);
+}
+
+Console.WriteLine("\nDungeon is ready. Enter now!");
