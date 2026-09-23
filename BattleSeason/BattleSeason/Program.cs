646a4a866bc9baa22218296229ea2e79f58fb4f0
AaronPrince33 <aaron.rayyan7718@gmail.com>
2026-09-23 15:11:53 +0700

    BattleSeason

--- BattleSeason/BattleSeason/Program.cs
+++ BattleSeason/BattleSeason/Program.cs
@@ -1,10 +1,24 @@
-﻿Console.Write("Input character's name: ");
-string name = Console.ReadLine();
+﻿string charName;
+int number0fBattle, expGained, totalEXP = 0;
+
+Console.Write("Input character's name: ");
+charName = Console.ReadLine();
 
 Console.Write("Imput the number of battles: ");
-int number = int.Parse(Console.ReadLine());
+number0fBattle = int.Parse(Console.ReadLine());
 
 Console.Write("Input the EXP gained per battle: ");
-int exp = int.Parse(Console.ReadLine());
+expGained = int.Parse(Console.ReadLine());
 
 Console.WriteLine("----------------------------");
+
+for (int i = 1; i <= number0fBattle; i++)
+{
+    totalEXP += expGained;
+    Console.WriteLine($"Battle {i} | EXP Gained: {expGained} | Total EXP: {totalEXP}");
+}
+
+Console.WriteLine("\n=== BATTLE COMPLETE ===");
+Console.WriteLine($"Character:\t{charName}");
+Console.WriteLine($"Battles:\t{number0fBattle}");
+Console.WriteLine($"Total EXP:\t{totalEXP}");
