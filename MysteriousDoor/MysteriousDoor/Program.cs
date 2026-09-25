6d8a101c5ff45352f80c93e777ba2021e89fd5aa
AaronPrince33 <aaron.rayyan7718@gmail.com>
2026-09-25 14:03:10 +0700

    MysteriousDoor

--- MysteriousDoor/MysteriousDoor/Program.cs
+++ MysteriousDoor/MysteriousDoor/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string secretCode = "1992";
+string attempt = "";
+
+while (attempt != secretCode)
+{
+    Console.Write("Enter the secret code: ");
+    attempt = Console.ReadLine();
+    if (attempt != secretCode)
+    {
+        Console.WriteLine("Wrong code, Try again!");
+    }
+}
+
+Console.WriteLine("Door Unlocked. Congratulations!!");
